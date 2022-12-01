6ef9b9e496f63a00b325220c7df815fa88a8de93
Younion <younion@gmail.com>
2022-11-30 17:30:32 -0700

    Added comments to organize

--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-var corsMiddleware = function(req, res, next) {
+// Apps
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(corsMiddleware);
+// app.use(cors());
+
+const corsMiddleware = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Origin');
@@ -11,14 +18,18 @@ var corsMiddleware = function(req, res, next) {
     next();
 }
 
+// Sequelize connect to DB
 const sequelize = new Sequelize(process.env.PSQL_SERVER, process.env.PSQL_USER, process.env.PSQL_PASS, {
     host: process.env.HOST,
     dialect: 'postgres'
 });
 
+// Models
 // const Jyh = require('./models/JyhModel')(sequelize);
 const Output = require('./models/QueryOutputModel')(sequelize);
+const TestTable = require('./models/TestTable')(sequelize);
 
+// Test Connection Authentication
 async function connectToPg() {
     try {
         await sequelize.authenticate();
@@ -30,20 +41,15 @@ async function connectToPg() {
 
 connectToPg();
 
-sequelize.sync();
-
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(corsMiddleware);
-
-// app.use(cors());
-
+// GET
 app.get('/', async (req, res) => { 
     const sauces = await Output.findAll()
         console.log(sauces);
         res.json(sauces);
 });
+
+// Sync Sequelize
+sequelize.sync();
 
 const port = process.env.DEV_PORT;
 app.listen(port, () => console.log(`Server running on port ${port}, http://localhost:${port}`));
--- server/models/TestTable.js
+++ server/models/TestTable.js
@@ -13,20 +13,18 @@ module.exports = function(sequelize) {
         freezeTableName: true, 
     });
     
-return TestTable;
-
-// TestTable.sync({ alter: true })
-//     .then(() => {
-//         return TestTable.create({
-//             test_name: "testsauce2",
-//             test_sauce: "testsauce2"
-//         });
-//     }).then((data) => {
-//         console.log("Data added to database!");
-//         console.log(data.toJSON())
-//     }).catch((err) => {
-//         console.log(err);
-//     });
+TestTable.sync({ alter: true })
+    .then(() => {
+        return TestTable.create({
+            test_name: "testsauce5",
+            test_sauce: "testsauce5"
+        });
+    }).then((data) => {
+        console.log("Data added to database!");
+        console.log(data.toJSON())
+    }).catch((err) => {
+        console.log(err);
+    });
 };
 
     // Associations
